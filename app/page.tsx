c055a60396a46e55f33b8422f0391971a83e2953
yaqoob-halepoto <yaqoobahmed45700@gmail.com>
2026-01-07 21:55:57 +0500

    feat: Initialize project with backend dependencies, utilities, and assets.

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/lading-page-comps/Container";
+import LandingPage from "@/components/lading-page-comps/LandingPage";
+import NavBar from "@/components/lading-page-comps/navBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <LandingPage />
+  );
+}
